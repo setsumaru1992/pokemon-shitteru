3a9c89abe4e5111c1daeb3ed67b9bdb442764ff2
setsumaru1992 <setsumaru1992@gmail.com>
2025-04-13 18:48:32 +0900

    vitestの並列実行の仕組みだと他のテストデータに干渉してユニークキー違反とか起きるから、基本的に逐次実行へ

--- src/backend/prisma/index.ts
+++ src/backend/prisma/index.ts
@@ -1,14 +1,26 @@
 import { PrismaClient } from "./generated/client";
 
+import type { LogLevel } from "./generated/client";
+
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
+const logLevels: LogLevel[] = ((): LogLevel[] => {
+  if (
+    process.env.NODE_ENV === "production" ||
+    process.env.NODE_ENV === "test"
+  ) {
+    return ["error", "warn"];
+  }
+  return ["query", "error", "warn"];
+})();
+
 const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ["query", "error", "warn"],
+    log: logLevels,
   });
 
 if (process.env.NODE_ENV !== "production") {
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vitest/config";
 import path from "path";
+
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
@@ -14,6 +15,12 @@ export default defineConfig({
     environmentOptions: {
       jsdom: {
         resources: "usable",
+      },
+    },
+    pool: "forks", // デフォルト
+    poolOptions: {
+      forks: {
+        singleFork: true, // 並列実行するとDBで他のテストデータが干渉するため基本は逐次実行
       },
     },
   },
